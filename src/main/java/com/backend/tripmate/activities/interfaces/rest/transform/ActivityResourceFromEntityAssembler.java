05587c503f90051703d887cadda3ee0aa0e30327
MesSobble <u201911727@upc.edu.pe>
2024-05-29 22:03:58 -0500

    fix: Assembler

--- src/main/java/com/backend/tripmate/activities/interfaces/rest/transform/ActivityResourceFromEntityAssembler.java
+++ src/main/java/com/backend/tripmate/activities/interfaces/rest/transform/ActivityResourceFromEntityAssembler.java
@@ -6,7 +6,6 @@ import com.backend.tripmate.activities.interfaces.rest.resources.ActivityResourc
 public class ActivityResourceFromEntityAssembler {
 
     public static ActivityResource toResourceFromEntity(Activity entity) {
-        return new ActivityResource(entity.getName(), entity.getImagePath(), entity.getDescription(), entity.getLocation(), entity.getPrice());
+        return new ActivityResource(entity.getId(), entity.getName(), entity.getImagePath(), entity.getDescription(), entity.getLocation(), entity.getPrice());
     }
-
 }
