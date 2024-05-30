e1a12b23d3251137e7bc6263caefe15ce33d0aca
MesSobble <u201911727@upc.edu.pe>
2024-05-29 21:54:52 -0500

    feat:add Controller and Services

---
+++ src/main/java/com/backend/tripmate/activities/ActivityController.java
@@ -0,0 +1,47 @@
+package com.backend.tripmate.activities;
+
+import com.backend.tripmate.activities.interfaces.rest.resources.ActivityResource;
+import com.backend.tripmate.activities.domain.model.queries.GetAllActivitiesQuery;
+import com.backend.tripmate.activities.domain.services.ActivityQueryService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//el list de retorno:
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+//import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping(value = "/api/v1/actividades", produces = APPLICATION_JSON_VALUE)
+//@Tag(name = "Activities", description = "Activity Management Endpoints")
+public class ActivityController {
+
+    //private final ActivityCommandService activityCommandService;
+    private final ActivityQueryService activityQueryService;
+
+
+    public ActivityController(ActivityQueryService activityQueryService) {
+        //this.activityCommandService = activityCommandService;
+        this.activityQueryService = activityQueryService;
+    }
+
+    //viewModel
+    public ResponseEntity<List<ActivityResource>> getAllActivities() {
+
+        //getquery
+        var getAllActivitiesQuery = new GetAllActivitiesQuery();
+
+        //hand
+        var activities = activityQueryService.handle(getAllActivitiesQuery);
+
+        //convert a ActivityResource
+        var activityResources = activities.stream().map(ActivityResourceFromEntityAssembler::toResourceFromEntity).toList();
+
+        //return.
+        return ResponseEntity.ok(activityResources);
+    }
+
+}
---
+++ src/main/java/com/backend/tripmate/activities/interfaces/rest/transform/ActivityResourceFromEntityAssembler.java
@@ -0,0 +1,12 @@
+package com.backend.tripmate.activities.interfaces.rest.transform;
+
+import com.backend.tripmate.activities.domain.model.entities.Activity;
+import com.backend.tripmate.activities.interfaces.rest.resources.ActivityResource;
+
+public class ActivityResourceFromEntityAssembler {
+
+    public static ActivityResource toResourceFromEntity(Activity entity) {
+        return new ActivityResource(entity.getName(), entity.getImagePath(), entity.getDescription(), entity.getLocation(), entity.getPrice());
+    }
+
+}
