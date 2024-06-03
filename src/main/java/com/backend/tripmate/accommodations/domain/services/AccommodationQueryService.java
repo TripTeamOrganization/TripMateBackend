0d5f8887a6cea2a345b28c8711f5e5d9bf32c8ea
paolomv02 <u202010039@upc.edu.pe>
2024-06-03 02:18:26 -0500

    fix: rename get all accommodations query

--- src/main/java/com/backend/tripmate/accommodations/domain/services/AccommodationQueryService.java
+++ src/main/java/com/backend/tripmate/accommodations/domain/services/AccommodationQueryService.java
@@ -1,11 +1,11 @@
 package com.backend.tripmate.accommodations.domain.services;
 
 import com.backend.tripmate.accommodations.domain.model.entities.Accommodation;
-import com.backend.tripmate.accommodations.domain.model.queries.GetAllAccommodations;
+import com.backend.tripmate.accommodations.domain.model.queries.GetAllAccommodationsQuery;
 
 import java.util.List;
 
 public interface AccommodationQueryService {
 
-    List<Accommodation> handle(GetAllAccommodations query);
+    List<Accommodation> handle(GetAllAccommodationsQuery query);
 }
