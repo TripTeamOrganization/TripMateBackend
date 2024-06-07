d47c02e303550f78142b9b1575fb8f6916e5c675
Leonel-Alfaro <u20201a930@upc.edu.pe>
2024-06-07 12:04:02 -0500

    fix: files

--- src/main/java/com/backend/tripmate/reservation/domain/exceptions/ReservationNotFoundException.java
+++ src/main/java/com/backend/tripmate/reservation/domain/exceptions/ReservationNotFoundException.java
@@ -1,4 +1,7 @@
 package com.backend.tripmate.reservation.domain.exceptions;
 
-public class ReservationNotFoundException {
+public class ReservationNotFoundException extends RuntimeException {
+    public ReservationNotFoundException(Long id) {
+        super("Reservation not found with id: " + id);
+    }
 }
